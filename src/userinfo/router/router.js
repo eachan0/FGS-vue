1fdc3c911cf5a2422e50fcf679830f054afd5801
zhuyc <zyc199777@outlook.com>
2019-02-19 23:38:54 +0800

    add router

--- src/userinfo/router/router.js
+++ src/userinfo/router/router.js
@@ -1,10 +1,11 @@
-// import Vue from 'vue';
-// import VueRouter  from "vue-router";
+import Vue from 'vue';
+import VueRouter from "vue-routerv"
 import Info from '@/userinfo/views/Info';
 import Pass from '@/userinfo/views/Pass';
 import Address from '@/userinfo/views/Address';
 
-// Vue.use(VueRouter);
-//
-// export default new VueRouter({
-// });
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes:[]
+});
